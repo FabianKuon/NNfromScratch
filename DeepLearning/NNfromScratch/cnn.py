fe0649a28626a353fe57b8c0d4335f60d3ac29f7
Fabian Kuon <kuonfabian@freenet.de>
2020-11-17 20:24:03 +0100

    further implementation

---
+++ DeepLearning/NNfromScratch/cnn.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class ConvNet():
+    def __init__(self, X, Y, lr):
+
+def padding(X, pad):
+    return X_pad = np.pad(X, ((0,0), (pad, pad), (0, 0)), 'constant',
+                          constant_values = (0,0))
+
--- DeepLearning/NNfromScratch/imageStream.py
+++ DeepLearning/NNfromScratch/imageStream.py
@@ -1,6 +1,7 @@
 import pickle
 import requests
 import os.path
+import os
 
 def FileWatch():
     if os.path.exists('cifar-10-batches-py'):
@@ -9,11 +10,17 @@ def FileWatch():
         url = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
         r = requests.get(url, allow_redirects=True)
         open('cifar-10-python.tar.gz', 'wb').write(r.content)
-        print('Downloading cifar data set...')
+        print('Cifar data set downloaded successfully...')
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
 def main():
     FileWatch()
+    dict = unpickle('cifar-10-batches-py/data_batch_1')
 
-if __name__=='main':
+if __name__=="__main__":
     main()
 
