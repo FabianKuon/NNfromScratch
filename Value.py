ff401a5eedbe28924f1fef20fda3a03917746b5b
Fabian Kuon <kuonfabian@freenet.de>
2022-11-05 18:57:16 +0100

    Feature: Added operation and children attribute to Values class.

--- Value.py
+++ Value.py
@@ -1,16 +1,20 @@
+from graphviz import Digraph
+
+
 class Value:
-    def __init__(self, data, _children=()):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
         self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other))
+        return Value(self.data + other.data, (self, other), '+')
 
     def __mul__(self, other):
-        return Value(self.data * other.value, (self, other))
+        return Value(self.data * other.value, (self, other), '*')
 
 
 if __name__ == "__main__":
