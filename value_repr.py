37bb48be65b920a947c568000dcd65cea7bd9936
Fabian Kuon <kuonfabian@freenet.de>
2022-11-07 21:12:48 +0100

    Added additional mathematical operations to Values class

--- value_repr.py
+++ value_repr.py
@@ -1,3 +1,4 @@
+"""Class representing a neural network implementation without a ML library"""
 import math
 from graphviz import Digraph, DOT_BINARY
 
@@ -19,39 +20,83 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        # enables addition of Value instance with e.g. integers
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
 
         out._backward = _backward
         return out
 
+    def __neg__(self):
+        return self * -1
+
+    def __subtr__(self, other):
+        return self * (-other)
+
     def __mul__(self, other):
+        # enables multiplication of Value instance with e.g. integers
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
 
         out._backward = _backward
         return out
+
+    def __pow__(self, other):
+        assert isinstance(
+            other, (int, float)), "only supporting int and float for power as of now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad = other * (self.data ** (other - 1)) * out.grad
+        out._backward = _backward
+        return out
+
+    def __rmul__(self, other):
+        """Function which allows for 2*a=2.__mul__(a) which would otherwise not be possible"""
+        return self * other
+
+    def __truediv__(self, other):
+        """
+        Function for division operation of Value instances
+        """
+        return self * other**-1
 
     def tanh(self):
         """
         Tanh evaluation of Values instance
         """
-        x = (math.exp(2*self.data) - 1)/(math.exp(2*self.data) + 1)
-        out = Value(x, (self, ), 'tanh')
+        tanh = (math.exp(2*self.data) - 1)/(math.exp(2*self.data) + 1)
+        out = Value(tanh, (self, ), 'tanh')
 
         def _backward():
-            self.grad = (1.0 - x**2) * out.grad
+            self.grad += (1.0 - tanh**2) * out.grad
 
         out._backward = _backward
         return out
 
+    def exp(self):
+        """
+        exp function for Values instance
+        """
+        out = Value(math.exp(self.data, (self,), 'exp'))
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+
     def backward(self):
+        """
+        Backward propagation through neural net
+        """
         topo = []
         visited = set()
 
@@ -95,8 +140,11 @@ def draw_dot(root: float) -> DOT_BINARY:
     for node in nodes:
         uid = str(id(node))
         # for any value in the graph, create a rectangular ('record') node for it
-        dot.node(name=uid, label="{ %s | data %.4f | grad %.4f }" %
-                 (node.label, node.data, node.grad, ), shape='record')
+        dot.node(
+            name=uid,
+            label=f"{{ {node.label} | data {node.data:.4f} | grad {node.grad:.4f} }}",
+            shape='record')
+
         if node._op:
             # if this value is a result of some operatoin, create an op node for it
             dot.node(name=uid + node._op, label=node._op)
