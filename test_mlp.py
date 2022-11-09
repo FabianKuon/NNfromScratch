d6e1ccff54919c13505122b56a37a3884ad15036
Fabian Kuon <kuonfabian@freenet.de>
2022-11-09 20:05:39 +0100

    creat a test class for the mlp

---
+++ test_mlp.py
@@ -0,0 +1,18 @@
+"""Training of a mlp with a simple data example"""
+from multiLayerPerceptron import MLP
+
+
+if __name__ == '__main__':
+    # a multi-layer-perceptron with input dimension 3, first and second layer dimension 4 and outpit dimension 1
+    n = MLP(3, [4, 4, 1])
+    features = [
+        [2.0, 3.0, -1.0],
+        [3.0, -1.0, 0.5],
+        [0.5, 1.0, 1.0],
+        [1.0, 1.0, -1.0]
+    ]
+
+    labels = [1.0, -1.0, -1.0, 1.0]
+    # n() calls the implemented __call__ method of the mlp, since n is a mlp instance
+    predictions = [n(x) for x in features]
+    print(predictions)
